eb776a4d44f1bd48bb50d7a84250654574eb8329
Amelie Paterson <dmp537@york.ac.uk>
2025-04-27 14:09:23 +0100

    Implement remove_task with index validation

--- python/todo.py
+++ python/todo.py
@@ -16,6 +16,10 @@ def add_task(task):
         file.write(f"{task}\n")
 
 def list_tasks():
+    """Function: list_task
+    
+    Returns a formatted string of all tasks with indices
+    """
     
     # Check if task file exists before trying to read
     if not os.path.exists(TASK_FILE):
@@ -43,8 +47,39 @@ def list_tasks():
     
 
 def remove_task(index):
-    return
-
+    """Function: remove_task
+    
+    Input - Index (starting at 1) of the task to remove
+    Return - nothing
+    """
+    
+    # Check if task file exists before tying to remove task
+    if not os.path.exists(TASK_FILE):
+        return
+    
+    try:
+        # Open and read tasks from file
+        with open(TASK_FILE, "r", encoding="utf-8") as file:
+            tasks = file.readlines()
+            
+        #check if index is positive and within range
+        if index <=0 or index >len(tasks):
+            return
+        
+        # Remove task from list 
+        # Convert from 1-based user index 0-based index
+        task_index = index - 1
+        tasks.pop(task_index)
+        
+        # Write the updated task list back to file
+        with open(TASK_FILE, "w", encoding="utf-8") as file:
+            file.writelines(task)
+            
+    except Exception:
+                # Return if errors occur
+                return
+    
+    
 def main():
     parser = argparse.ArgumentParser(description="Command-line Todo List")
     parser.add_argument(
